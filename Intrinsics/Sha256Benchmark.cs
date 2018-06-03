d40e0600bff8a34fc59ae8243125552f734dad5a
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-06-03 14:08:12 +0200

    Update benchmarks

--- Intrinsics/Sha256Benchmark.cs
+++ Intrinsics/Sha256Benchmark.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Attributes.Jobs;
@@ -9,25 +10,21 @@ namespace Intrinsics
 	[InProcess]
 	public class Sha256Benchmark
 	{
-		private static readonly byte[] data = new byte[1024 * 1024];
+		private static readonly byte[] data1K = new byte[1024];
+		private static readonly byte[] data1M = new byte[1024 * 1024];
 
 		private readonly SHA256 openSsl = SHA256.Create();
 		private readonly Sha256Digest bouncyCastle = new Sha256Digest();
 
-		[Benchmark]
-		public byte[] OpenSsl()
-		{
-			return openSsl.ComputeHash(data);
-		}
+		[Benchmark] public byte[] OpenSsl1K() => openSsl.ComputeHash(data1K);
+		[Benchmark] public byte[] Intrinsics1K() => Sha256Arm64.ComputeHash(data1K);
+		[Benchmark] public byte[] BouncyCastle1K() => BouncyCastle(data1K);
 
-		[Benchmark]
-		public byte[] Intrinsics()
-		{
-			return Sha256Arm64.ComputeHash(data);
-		}
+		[Benchmark] public byte[] OpenSsl1M() => openSsl.ComputeHash(data1M);
+		[Benchmark] public byte[] Intrinsics1M() => Sha256Arm64.ComputeHash(data1M);
+		[Benchmark] public byte[] BouncyCastle1M() => BouncyCastle(data1M);
 
-		[Benchmark]
-		public byte[] BouncyCastle()
+		private byte[] BouncyCastle(byte[] data)
 		{
 			byte[] digest = new byte[bouncyCastle.GetDigestSize()];
 
